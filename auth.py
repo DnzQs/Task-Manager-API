ca897ca24386b0a0cf6eb61cbd7d306b5c806024
Denis Kolesnichenko <dentv2017@gmail.com>
2026-04-30 18:22:11 +0300

    Add files via upload

--- auth.py
+++ auth.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from db import get_db
-from models import User
-from schemas import UserCreate
-from utils import hash_password
+from app_API.db import get_db
+from app_API.models import User
+from app_API.schemas import UserCreate
+from app_API.utils import hash_password, create_access_token
+from test1.auth import verify_password
 
 router = APIRouter()
 
@@ -24,3 +25,18 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
 
     return {"message": "User created"}
+
+
+@router.post("/login")
+def login(email: str, password: str, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email == email).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="Incorrect email or password")
+
+    if not verify_password(password, user.hashed_password):
+        return {"message": "Incorrect credentials"}
+
+    token = create_access_token({"sub": user.email})
+
+    return {"access_token": token, "token_type": "bearer"}
