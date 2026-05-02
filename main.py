134e953edea1ff68fd37d6f421079f02b4bf72cd
Denis Kolesnichenko <dentv2017@gmail.com>
2026-05-02 21:17:31 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from fastapi import FastAPI, HTTPException
 from app_API.db import Base, engine
 from app_API.models import User
 from app_API.routes import auth
+from app_API.routes import task
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.include_router(auth.router, prefix="/auth")
+
+app.include_router(task.router, prefix="/task", tags=["task"])
 
 
 
